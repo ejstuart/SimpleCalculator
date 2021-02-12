03b9e3cfbef95ac2c9135ee2d79bbd9ecd6733e0
ejstuart <estu836@aucklanduni.ac.nz>
2021-02-12 22:11:04 +1300

    Created some backend classes as scaffolding

---
+++ src/backend/Calculations.java
@@ -0,0 +1,11 @@
+package backend;
+
+/**
+ * Class to handle the basic calculations (and potentially some more complex ones in the future), such as addition, subtraction
+ * etc.
+ */
+public class Calculations {
+
+    private operator
+
+}
